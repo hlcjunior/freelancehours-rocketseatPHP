43e650e1cc8dd328ec3d760f3266303d65714a93
hlcjunior <62648013+hlcjunior@users.noreply.github.com>
2024-11-11 18:18:54 -0300

    Aula 2 OK

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -25,7 +25,7 @@ class ProjectFactory extends Factory
             'description'=>fake()->randomHtml(),
             'ends_at'=>fake()->dateTimeBetween('now', '+3 days'),
             'status'=>fake()->randomElement(['open', 'closed']),
-            'tech_stack'=>fake()->randomElements(['react','php','laravel','vue','tailwind','javascript','nextjs','python'],random_int(1,5)),
+            'tech_stack'=>fake()->randomElements(['react','vite','nodejs','javascript','nextjs'],random_int(1,5)),
             'created_by'=>User::Factory(),
         ];
     }
