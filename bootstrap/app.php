02cb8c7395e308cc9309d0822659ca6a8cdf63d0
hlcjunior <62648013+hlcjunior@users.noreply.github.com>
2024-11-22 17:58:56 -0300

    Adicionado comando

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withSchedule(function (\Illuminate\Console\Scheduling\Schedule $schedule) {
+        $schedule->command('app:close-project')->everyMinute();
+    })
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
